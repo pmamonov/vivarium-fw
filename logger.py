78ebeb26f964880f6ab864f516feeaafea58eca0
piton <unknown>
2014-01-28 17:50:23 +0400

    Improve device disconnect in logger.py. Added web interface.

--- logger.py
+++ logger.py
@@ -1,14 +1,107 @@
 #!/usr/bin/python
-import sys, serial, time
+import os, sys, serial, time, argparse, glob, random, signal, ctypes
+dev_pat = "/dev/LOGGER*"
 
-sr = serial.Serial(port=sys.argv[1], timeout=0.25)
+libc = ctypes.cdll.LoadLibrary("libc.so.6")
+mask = '\x00' * 17 # 16 byte empty mask + null terminator 
+libc.sigprocmask(2, mask, None)
+
+def connect(dev = None, ntry=-1):
+  if dev == None:
+    while 1:
+      dev = dev_find()
+      if dev: break
+      else:
+        print "Device not found"
+        time.sleep(1)
+      if ntry > 0: ntry-=1
+      if ntry ==0: raise NameError, "Device not found"
+  sr = serial.Serial(port=dev, timeout=0.5, baudrate=115200)
+  while len(sr.read(1))>0: pass
+  return sr
+ 
+def dev_find():
+  d = glob.glob(dev_pat)
+  if not len(d): return None # raise NameError, "Device not found"
+  return d[-1] # Only one device is supported
+
+class FakeSerial:
+  def __init__(self, *args):
+    pass
+
+  def readline(self):
+    return reduce(lambda a,b: a+b, map(lambda n: "%d "%n, map(lambda i: random.randint(0,4096), range(4))))
+
+  def close(self):
+    pass
+
+  def write(self,s):
+    pass
+
+prs = argparse.ArgumentParser(description="")
+prs.add_argument('-p', dest="lockf", metavar='FILENAME', type=str, help='lock file', default='logger.pid')
+prs.add_argument('-t', dest='period', metavar='N', type=float, help='data collection period',default=1.0)
+prs.add_argument('-o', dest="out", metavar='FILE', type=str, help='output file',default='logger.dat')
+prs.add_argument('-f', dest="dev", metavar='/dev/ttyX', type=str, help='device file')
+prs.add_argument('-d', dest="daemon",  action="store_true", help='daemonize')
+prs.add_argument('-l', dest="log", metavar='FILE', type=str, help='log file',default='logger.log')
+args = prs.parse_args()
+
+def bye(sig,fr):
+  print "%s Logger stopped"%time.strftime("%Y-%m-%d_%H:%M:%S")
+  sys.exit(0)
+
+
+if args.daemon:
+  if os.fork() > 0:
+    sys.exit(0)
+  os.setsid()
+  if os.fork() > 0: sys.exit(0)
+  os.umask(0)
+  import resource		# Resource usage information.
+  maxfd = resource.getrlimit(resource.RLIMIT_NOFILE)[1]
+  if (maxfd == resource.RLIM_INFINITY):
+    maxfd = 1024
+  for fd in range(0, maxfd):
+    try:
+      os.close(fd)
+    except OSError:	# ERROR, fd wasn't open to begin with (ignored)
+      pass
+
+signal.signal(signal.SIGTERM,bye)
+
+sys.stdin = open('/dev/null', 'r')
+sys.stdout = open(args.log, 'a+',0)
+sys.stderr = open(args.log, 'a+',0)
+
+
+lck = open(args.lockf, 'w')
+lck.write(str(os.getpid()))
+lck.close()
+out = open(args.out, 'w')
+#dev = args.dev if args.dev else dev_find()
+sr = connect(args.dev, ntry=1)
+#while len(sr.read(1))>0: pass
+#sr = FakeSerial()
+
+
+tstart=time.time()
+print "%s Logger started"%time.strftime("%Y-%m-%d_%H:%M:%S")
 while True:
-  sr.write("get\n")
-  s=""
-  while True:
-    s+=sr.read()
-    if s[-1] == '\n':
-      print "%.2f  %s"%(time.time(), s),
-      sys.stdout.flush()
-      break
-  time.sleep(0.1)
+  try:
+    sr.flushInput()
+    sr.write("get\n")
+    s = sr.readline()
+  except (serial.serialutil.SerialException, serial.termios.error) as err:
+    print "%s Error communicating to device: %s"%(time.strftime("%Y-%m-%d_%H:%M:%S"),str(err))
+    sr.close()
+    time.sleep(1)
+#      sys.stdout.flush()
+    sr = connect() #serial.Serial(port=dev_find(), timeout=0.5)
+#    while len(sr.read(1))>0: pass
+    print "%s Reconnected to device"%time.strftime("%Y-%m-%d_%H:%M:%S")
+  out.write("%.2f"%(time.time()-tstart))
+  out.write(reduce(lambda a,b: a+b, map(lambda v: " %s"%v, s.split()))+"\n")
+  out.flush()
+#  sys.stdout.flush()
+  time.sleep(args.period)
---
+++ weblogger.py
@@ -0,0 +1,135 @@
+import glob, time, os, mod_python, numpy, sys
+rootdir = "/var/www/logger/"
+wwwroot = "/logger/"
+logger = "/home/pi/logger.py"
+
+def is_running():
+  if not os.path.isfile(rootdir+"logger.pid"): return 0
+  f=open(rootdir+"logger.pid",'r')
+  pid=f.readline()
+  f.close()
+  if not os.path.isfile("/proc/%s/cmdline"%pid): return 0
+  f=open("/proc/%s/cmdline"%pid,'r')
+  cmd = f.readline().split('\x00')
+  if len(cmd)<2: return 0
+  if not (cmd[0]=='python' and cmd[1].split('/')[-1]=='logger.py'): return 0
+  return int(pid)
+
+
+def index(req):
+  s="<html><body>"
+  if is_running():
+    s+="<h2>Logger is running</h2>"
+    s+='<form method="get" action="'+wwwroot+"weblogger.py/stop"+ '"> <input type="submit" value="STOP"></form><hr>'
+  else:
+    s+="<h2>Logger is stopped</h2>"
+    s+='<form method="get" action="'+wwwroot+"weblogger.py/start"+ '"><input type="text" name="period"><input type="submit" value="START"></form><hr>'
+  dat = glob.glob(rootdir+"*.dat")
+  dat.sort(reverse=1)
+  for f in dat:
+    fn = f.split('/')[-1]
+    s+="<a href=%s>%s</a>  <a href='%s' style='color:green'>download</a>  <a href='%s' style='color:red;'>delete</a><hr>"%(wwwroot+"weblogger.py/view?fn=%s"%fn, fn, wwwroot+fn,wwwroot+"weblogger.py/delete?fn=%s"%fn)
+  return s+"</body></html>"
+
+def start(req,period):
+  if not is_running():
+    dat = rootdir+"%s.dat"%time.strftime("%Y-%m-%d_%H:%M:%S")
+    lockf = rootdir+"logger.pid"
+    log = rootdir+"logger.log"
+#    cmdlogger = "python "+logger+" -t "+period+" -p "+lockf+" -o "+dat
+#    cmd = "(echo `date` START; echo "+cmdlogger+"; "+cmdlogger+"; echo `date` STOP) >> "+log+" 2>&1 &"
+    os.system(reduce(lambda a,b: a+" "+b, (logger,'-t',period,'-p',lockf,'-o',dat,'-l',log,'-d'),'python'))
+  time.sleep(1)
+  mod_python.util.redirect(req,wwwroot+"weblogger.py")
+
+def stop(req):
+  pid = is_running()
+  if pid: os.kill(pid,15)
+  time.sleep(1)
+  mod_python.util.redirect(req,wwwroot+"weblogger.py")
+
+def view(req,fn):
+  s="""
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+      google.load("visualization", "1", {packages:["corechart"]});
+      google.setOnLoadCallback(drawChart);
+      var data;
+      var chart;
+      var options;
+      var scale=[];
+      function drawChart() {
+        data = google.visualization.arrayToDataTable([
+  """
+#  d = numpy.loadtxt(rootdir+fn)#[-1000:,:]
+  d = open(rootdir+fn)
+  ncol = len(d.readline().split(' '))
+#  dt = float(d.readline().split(' ')[0])
+  nr=2
+  while d.readline(): nr +=1
+  d.seek(0)
+#  dn = int(10./dt)
+  dn = int(nr/1e3)
+  if dn==0: dn=1
+#  if nr<=1000: dn=1
+  s+="['time'"
+  s+=reduce(lambda a,i: "%s,'%d'"%(a,i), range(1,ncol),'')
+  s+=']'
+
+  il=0
+  for l in d: 
+    if il%dn == 0: s += ",[%s]"%(l.strip().replace(' ',','))
+#    if il>=100: break
+    il+=1
+  d.close()
+#  for i in xrange(d.shape[0]):
+#    s+='[%.2f'%d[i,0]
+#    s+=reduce(lambda a,b: "%s,%d"%(a,b),d[i,1:],'')
+#    s+=']'
+#    if i<d.shape[0]-1: s+=',\n'
+  s+="""
+        ]);
+
+        options = {
+          title: 'ADC values',
+//          explorer: {axis: 'horizontal', maxZoomIn: 0.01}
+          explorer: {maxZoomIn: 0.01}
+        };
+
+        for (var i=1; i<data.getNumberOfColumns(); i++){scale.push(1);};
+
+        chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+
+      function scale_data(){
+        nr = data.getNumberOfRows();
+        for (var c=1; c<data.getNumberOfColumns(); c++){
+          var s = parseFloat(document.getElementById("scale"+c.toString()).value);
+          for (var i=0; i<nr; i++){
+            data.setValue(i,c,scale[c-1]/s*data.getValue(i,c));
+          };
+          scale[c-1]=s;
+        }
+        chart.draw(data, options)
+      }
+    </script>
+  </head>
+  <body>
+  """
+  s += "<b>units per gram: </b>"
+  s += reduce(lambda a,b: a+b, map(lambda i: '%d:<input type="text" size="5" id="scale%d" value="1"> '%(i,i), range(1,ncol)))
+  s+= '<input type="button" value="Apply" onClick="scale_data();">'
+  s+="""
+    <div id="chart_div" style="width: 100%; height: 100%;"></div>
+  </body>
+</html>
+  """
+  return s
+
+def delete(req,fn):
+  if os.path.isfile(rootdir+fn): os.unlink(rootdir+fn)
+  mod_python.util.redirect(req,wwwroot+"weblogger.py")
+
