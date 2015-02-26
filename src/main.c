fe31f8f02583285599b885556ab68b2caa692a55
Peter Mamonov <pmamonov@gmail.com>
2015-02-26 23:14:09 +0300

    main.c: fix formatting

--- src/main.c
+++ src/main.c
@@ -4,71 +4,71 @@
 #include "usb_pwr.h"
 #include "stm32f10x_gpio.h"
 #include "stdlib.h"
-//#include "string.h"
 #include "cdcio.h"
 #include "FreeRTOS.h"
 #include "task.h"
-//#include "semphr.h"
-
 #include "strtok.h"
 #include "blink.h"
 #include "adc.h"
 #include "chat.h"
 
+int main(void)
+{
+	portBASE_TYPE err;
+	char s[64];
 
-int main(void){
-  portBASE_TYPE err;
-  char s[64];
+	Set_System();
 
-//  SystemInit();
-  Set_System();
+	Set_USBClock();
+	USB_Interrupts_Config();
+	USB_Init();
 
-  // USB setup
-  Set_USBClock();
-  USB_Interrupts_Config();
-  USB_Init();
+	err = xTaskCreate(vBlinkTask, "blink", 64, NULL,
+			  tskIDLE_PRIORITY + 1, NULL );
+	if ( err == pdPASS)
+		cdc_write_buf(&cdc_out, "blink started\n", 0, 1);
+	else {
+		sniprintf(s, sizeof(s),"blink failed %d", err);
+		cdc_write_buf(&cdc_out, s, 0,1);
+	}
 
+	err = xTaskCreate(vADCTask, "adc", 512, NULL,
+			  tskIDLE_PRIORITY + 1, NULL);
+	if (err == pdPASS)
+		cdc_write_buf(&cdc_out, "adc started\n", 0, 1);
+	else{
+		sniprintf(s, sizeof(s),"adc failed %d", err);
+		cdc_write_buf(&cdc_out, s, 0, 1);
+	}
 
-  err = xTaskCreate( vBlinkTask, "blink", 64, NULL, tskIDLE_PRIORITY+1, NULL );
-  if ( err == pdPASS)
-    cdc_write_buf(&cdc_out, "blink started\n", 0,1);
-  else{
-    sniprintf(s, sizeof(s),"blink failed %d", err);
-    cdc_write_buf(&cdc_out, s, 0,1);
-  }
+	err = xTaskCreate(vChatTask, "chat", 512, NULL,
+			  tskIDLE_PRIORITY + 1, NULL );
+	if (err == pdPASS)
+		cdc_write_buf(&cdc_out, "chat started\n", 0, 1);
+	else{
+		sniprintf(s, sizeof(s),"chat failed %d", err);
+		cdc_write_buf(&cdc_out, s, 0, 1);
+	}
 
-  err = xTaskCreate( vADCTask, "adc", 512, NULL, tskIDLE_PRIORITY+1, NULL );
-  if (err == pdPASS)
-    cdc_write_buf(&cdc_out, "adc started\n", 0,1);
-  else{
-    sniprintf(s, sizeof(s),"adc failed %d", err);
-    cdc_write_buf(&cdc_out, s, 0,1);
-  }
+	vTaskStartScheduler();
 
-  err = xTaskCreate( vChatTask, "chat", 512, NULL, tskIDLE_PRIORITY+1, NULL );
-  if (err == pdPASS)
-    cdc_write_buf(&cdc_out, "chat started\n", 0,1);
-  else{
-    sniprintf(s, sizeof(s),"chat failed %d", err);
-    cdc_write_buf(&cdc_out, s, 0,1);
-  }
-
-  vTaskStartScheduler();
-
-  while(1);
+	while(1);
 }
 
-void vApplicationStackOverflowHook( xTaskHandle xTask, signed portCHAR *pcTaskName ){
-  while(1);
+void vApplicationStackOverflowHook(xTaskHandle xTask,
+			           signed portCHAR *pcTaskName )
+{
+	while(1);
 }
+
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t* file, uint32_t line)
 {
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+/* User can add his own implementation to report the file name and line number,
+ ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {}
+	/* Infinite loop */
+	while (1)
+	{}
 }
 #endif
